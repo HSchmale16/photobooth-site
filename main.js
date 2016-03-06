4238ae0bfdbb7a25d0b70ff8de0449d305d59415
Henry J Schmale <hschmale16@gmail.com>
2016-03-06 12:56:05 -0500

    Much Better Camera Access
    I have to access the camera using the getMedia Api, because photobooth.js is
    too slow, and clunky for my taste. Based around a tutorial I found.

--- main.js
+++ main.js
@@ -3,11 +3,48 @@
  * Created by hschmale on 3/6/16.
  */
 
-var pb;
+var PhotoBooth = {
+    onMediaStream: function(stream) {
+        PhotoBooth.canvas = $('canvas')[0];
+        PhotoBooth.context = PhotoBooth.canvas.getContext('2d');
 
-function init(){
-    container = document.getElementById('photobooth');
+        PhotoBooth.localVideo = $('video')[0];
+        PhotoBooth.localVideo.src = window.URL.createObjectURL(stream);
+    },
+    noStream: function() {
+        console.log('FAIL TO GET WEBCAM ACCESS');
+    }
+};
 
-    pb = new Photobooth(container);
+function countdown(secs){
+    var countdownDiv = $('.countdown');
+    if(secs == 0){
+        countdownDiv.hide();
+        return;
+    }
+    countdownDiv.html(secs);
+    secs--;
+    setTimeout(function() {
+        countdown(secs);
+    }, 1000);
+}
+
+function init() {
+    $('#takePicture').click(takePicture);
+
+    getUserMedia(
+        {video: true},
+        PhotoBooth.onMediaStream,
+        PhotoBooth.noStream
+    );
+
+    function takePicture() {
+        $('.countdown').show();
+        countdown(3);
+        setTimeout(function() {
+            PhotoBooth.context.drawImage(PhotoBooth.localVideo, 0, 0, 200, 150);
+            $('#preview').show();
+        }, 3000);
+    }
 }
 
