0b58f866e6c15ec5da80b15cd4387bdbc040b5fc
Henry J Schmale <hschmale16@gmail.com>
2016-03-06 11:35:34 -0500

    Worked on the photobooth
    * Add jquery library
    * Create the photobooth iframe
    * Add CSS File for styling

---
+++ main.js
@@ -0,0 +1,13 @@
+/**
+ * main.js
+ * Created by hschmale on 3/6/16.
+ */
+
+var pb;
+
+function init(){
+    container = document.getElementById('photobooth');
+
+    pb = new Photobooth(container);
+}
+
