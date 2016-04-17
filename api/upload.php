63db6bf497694bde72c382475d1f980592528fa3
Henry J Schmale <hschmale16@gmail.com>
2016-04-17 12:08:49 -0400

    Add From for print email

--- api/upload.php
+++ api/upload.php
@@ -137,6 +137,7 @@ if(!$mail->send()){
 // send the email to the printer
 // There must be no body in the email, just the attachment to print.
 $printMail = new PHPMailer;
+$printMail->setFrom('photobooth@henryschmale.org');
 $printMail->addAddress('schmalePrint@hpeprint.com');
 $printMail->Subject = 'Newpaper Photobooth Email';
 $printMail->addAttachment($pdfFile);
