c51fca8acb1a13ed0d63294e49d82fae3518951f
Henry J Schmale <hschmale16@gmail.com>
2016-04-27 20:06:32 +0000

    Sent the emails. Finish the sending script.
    I finished writing the sending script. Correcting for errors that must be fixed to run on my webhost. Also add node_modules to gitignore since email script was written in node.js.

--- api/json/scanJSON4Email.js
+++ api/json/scanJSON4Email.js
@@ -1,14 +1,15 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 var dir = require('node-dir');
 var fs = require('fs');
 var _ = require('underscore');
-var sleep = require('sleep');
 var nodemailer = require('nodemailer');
 var sendmailTransport = require('nodemailer-sendmail-transport');
 
 // global message data
 var users = {emails: []};
+
+// create the mail transporter
 var transporter = nodemailer.createTransport(sendmailTransport({
     path: '/usr/bin/sendmail'
 }));
@@ -18,16 +19,23 @@ function endsWith(str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
-function sendEmail(address){
-    var msg = fs.readFileSync('/home/public/private/message.txt'); 
+function sendEmail(address) {
+    var msg = fs.readFileSync('message.txt', 'utf8'); 
 
     var mailOpts = {
         from: 'Barbra Birthday Photobooth <no-reply@henryschmale.org>',
-        to: 'hschmale16@gmail.com',
+        to: address,
         subject: 'Barbara\'s Birthday Party Photos are up',
         text: msg
     };
-    console.log(mailOpts); 
+    
+    // send the message
+    transporter.sendMail(mailOpts, function(err, info) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log('msg sent: ' + info.response);
+    });
 }
 
 
@@ -39,12 +47,17 @@ dir.files(__dirname, function(err, files) {
             var data = fs.readFileSync(files[path], 'utf8');
             if(!data){continue;}
             var json = JSON.parse(data);
-            json.email.forEach(function(email){
-                users.emails.push(email.trim());
-            });
+            if(json && json.email){
+                json.email.forEach(function(email){
+                    users.emails.push(email.trim());
+                });
+            }
         }
     }
     users.emails = _.uniq(users.emails);
+    users.emails.forEach(function(email){
+        sendEmail(email)
+    });
     console.log(users);
 });
 
